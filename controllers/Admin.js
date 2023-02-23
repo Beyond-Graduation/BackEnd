3d80abb7bc81d082ddefd0771b948c318c1c4883
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2023-02-24 00:40:01 +0530

    Added forgot password and resetting routes along with email support for every password activities

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -14,212 +14,206 @@ const { SECRET = "secret" } = process.env;
 
 
 
-router.get("/pending_alumni_list", isAdminLoggedIn, async (req, res) => {
+router.get("/pending_alumni_list", isAdminLoggedIn, async(req, res) => {
     const { AlumniPending } = req.context.models;
-    if(req.query.department){
-      var dept  = req.query.department
-      console.log("Department:",dept)
-      res.json(
-        await AlumniPending.find({department : dept}).lean().sort({dateJoined:-1}).catch((error) =>
-          res.status(400).json({ error })
-        )
-      );
+    if (req.query.department) {
+        var dept = req.query.department
+        console.log("Department:", dept)
+        res.json(
+            await AlumniPending.find({ department: dept }).lean().sort({ dateJoined: -1 }).catch((error) =>
+                res.status(400).json({ error })
+            )
+        );
+    } else {
+        res.json(
+            await AlumniPending.find().lean().sort({ dateJoined: -1 }).catch((error) =>
+                res.status(400).json({ error })
+            )
+        );
     }
-    else{
-      res.json(
-        await AlumniPending.find().lean().sort({dateJoined:-1}).catch((error) =>
-          res.status(400).json({ error })
-        )
-      );
-    }
-  });
+});
 
 
-router.post("/alumni_approve",isAdminLoggedIn,async(req,res)=>{
-  const { AlumniPending }= req.context.models;
-  var query = {}
-  if(req.body.userId){
-    curUserId = req.body.userId
-    let user = await AlumniPending.findOne({userId: curUserId  }).lean();
-    
-    if(user){
-        
-        const transporter = nodemailer.createTransport({
-          service:"Gmail",
-          auth:{
-              user:process.env.MAIL_ID,
-              pass:process.env.MAIL_PASSWORD
-          }
-        });
-        if(req.body.approved==0){
-          if(req.body.remarks && req.body.remarks.length>10){
-              const options={
-                from:process.env.MAIL_ID,
-                to:user.email,
-                subject:"Your Profile Registration application has been rejected!",
-                text:"Hi "+user.firstName+",\nYour Profile Registration application has been rejected as we couldn't verify your profile.\nRemarks:"+req.body.remarks+"\nKindly re-register or contact us for solving the issue if you are an Alumni of CET.\n\nRegards,\nBeyond Graduation,\nCETAA"
-              }
-              transporter.sendMail(options,function(err,info){
-                if(err){ 
-                    console.log(err);
-                    return;
+router.post("/alumni_approve", isAdminLoggedIn, async(req, res) => {
+    const { AlumniPending } = req.context.models;
+    var query = {}
+    if (req.body.userId) {
+        curUserId = req.body.userId
+        let user = await AlumniPending.findOne({ userId: curUserId }).lean();
+
+        if (user) {
+
+            const transporter = nodemailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: process.env.MAIL_ID,
+                    pass: process.env.MAIL_PASSWORD
                 }
-                console.log("Sent :" + info.response)
-              })
-              await AlumniPending.remove({userId: curUserId  });
-              res.send("User registration request rejected for "+user.firstName+".");
-              return;
+            });
+            if (req.body.approved == 0) {
+                if (req.body.remarks && req.body.remarks.length > 10) {
+                    const options = {
+                        from: process.env.MAIL_ID,
+                        to: user.email,
+                        subject: "Your Profile Registration application has been rejected!",
+                        text: "Hi " + user.firstName + ",\nYour Profile Registration application has been rejected as we couldn't verify your profile.\nRemarks:" + req.body.remarks + "\nKindly re-register or contact us for solving the issue if you are an Alumni of CET.\n\nRegards,\nBeyond Graduation,\nCETAA"
+                    }
+                    transporter.sendMail(options, function(err, info) {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                        console.log("Sent :" + info.response)
+                    })
+                    await AlumniPending.remove({ userId: curUserId });
+                    res.send("User registration request rejected for " + user.firstName + ".");
+                    return;
+                } else {
+                    res.send("Kindly add remarks of more than 10 characters to reject a User");
+                }
+            } else {
+                const { Alumni } = req.context.models;
+                delete user._id;
+                delete user.__t;
+                await AlumniPending.remove({ userId: curUserId });
+                updatedUser = await Alumni.create(user);
+
+                const options = {
+                    from: process.env.MAIL_ID,
+                    to: updatedUser.email,
+                    subject: "Your Alumni Profile has been Verified",
+                    text: "Hi " + updatedUser.firstName + ",\nThanks for registering to Beyond Grad! We have officially verified you as an Alumni and are looking forward to your valuable contribution. Kindly go ahead and login :)\n\nRegards,\nBeyond Graduation,\nCETAA"
+
+                }
+                transporter.sendMail(options, function(err, info) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log("Sent :" + info.response)
+                })
+
+                res.json(updatedUser);
             }
-            else{
-              res.send("Kindly add remarks of more than 10 characters to reject a User");
-            }
-          }
-          
-        else{
-          const { Alumni }= req.context.models;
-          delete user._id;
-          delete user.__t;
-          await AlumniPending.remove({userId: curUserId  });
-          updatedUser = await Alumni.create(user);
-          
-          const options={
-            from:process.env.MAIL_ID,
-            to:updatedUser.email,
-            subject:"Your Alumni Profile has been Verified",
-            text:"Hi "+updatedUser.firstName+",\nThanks for registering to Beyond Grad! We have officially verified you as an Alumni and are looking forward to your valuable contribution. Kindly go ahead and login :)\n\nRegards,\nBeyond Graduation,\nCETAA"
-        
-          }
-          transporter.sendMail(options,function(err,info){
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log("Sent :" + info.response)
-          })
-          
-          res.json(updatedUser);
+
+        } else {
+            res.status(400).json({ error: req.body.userId + " is not a pending alumni" });
         }
-        
+
     }
-    else{
-      res.status(400).json({ error: req.body.userId + " is not a pending alumni" });
-    }
-    
-  }
 });
 
 
 // Signup route to create a new user
-router.post("/signup", async (req, res) => {
+router.post("/signup", async(req, res) => {
     const { Admin } = req.context.models;
     try {
-      // hash the password
-      req.body.password = await bcrypt.hash(req.body.password, 10);
-      req.body.updated = Date.now();
-      req.body.likedBlogs=[]
-      // create a new user
-      const user = await Admin.create(req.body);
-      // send new user as response
-      res.json(user);
+        // hash the password
+        req.body.password = await bcrypt.hash(req.body.password, 10);
+        req.body.updated = Date.now();
+        req.body.likedBlogs = []
+            // create a new user
+        const user = await Admin.create(req.body);
+        // send new user as response
+        res.json(user);
     } catch (error) {
-      res.status(400).json({ error });
+        res.status(400).json({ error });
     }
-  });
-
-
-
-router.get("/admin_details", isAdminLoggedIn, async (req, res) => {
-  const { Admin } = req.context.models;
-
-  if(req.query.userId){
-    var curUserId  = req.query.userId
-    console.log("User Id:",curUserId)
-    res.json(
-      await Admin.findOne({userId:curUserId}).catch((error) =>
-        res.status(400).json({ error })
-      )
-    );
-  }
-});  
-
-router.post("/notice_approve",isAdminLoggedIn,async(req,res)=>{
-  const { NoticePending }= req.context.models;
-  var query = {}
-  if(req.body.noticeId){
-    curNoticeId = req.body.noticeId
-    let notice = await NoticePending.findOne({noticeId: curNoticeId  }).lean();
-    const { Alumni }= req.context.models;
-    var alumniDetails = await Alumni.findOne({userId:notice.userId}).lean();
-    if(notice){
-        const { Notice }= req.context.models;
-        delete notice._id;
-        await NoticePending.remove({noticeId: curNoticeId  });
-        const transporter = nodemailer.createTransport({
-          service:"Gmail",
-          auth:{
-              user:process.env.MAIL_ID,
-              pass:process.env.MAIL_PASSWORD
-          }
-        });
-        if(req.body.approved==0){
-          const options={
-            from:process.env.MAIL_ID,
-            to:alumniDetails.email,
-            subject:"Your Notice has been Rejected",
-            text:"Hi "+alumniDetails.firstName+",\n Sorry to inform that your Notice titled "+notice.title+" has been Rejected. Kindly Contact the admin to know more.\n\nRegards,\nBeyond Graduation,\nCETAA"
-        
-          }
-          transporter.sendMail(options,function(err,info){
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log("Sent :" + info.response)
-          })
-          res.send("Notice request rejected for "+alumniDetails.firstName+".");
-          return;
-        }
-        else{
-          var updatedNotice = await Notice.create(notice);
-          const options={
-            from:process.env.MAIL_ID,
-            to:alumniDetails.email,
-            subject:"Your Notice has been Approved",
-            text:"Hi "+alumniDetails.firstName+",\n Your notice titled "+notice.title+" has been Approved. Thanks for your contribution :)\n\nRegards,\nBeyond Graduation,\nCETAA"
-        
-          }
-          transporter.sendMail(options,function(err,info){
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log("Sent :" + info.response)
-          })
-          res.send("Notice request approved for "+alumniDetails.firstName+".");
-          return;
-        }
-        
-    }
-    res.json(updatedNotice);
-  }
 });
 
-router.get("/pending_notice_list", isAdminLoggedIn, async (req, res) => {
-  const { NoticePending } = req.context.models;
-  res.json(
-      await NoticePending.find().lean().sort({dateUploaded:-1,title:1}).catch((error) =>
-        res.status(400).json({ error })
-      )
+
+
+router.get("/admin_details", isAdminLoggedIn, async(req, res) => {
+    const { Admin } = req.context.models;
+
+    if (req.query.userId) {
+        var curUserId = req.query.userId
+        console.log("User Id:", curUserId)
+        res.json(
+            await Admin.findOne({ userId: curUserId }).catch((error) =>
+                res.status(400).json({ error })
+            )
+        );
+    }
+});
+
+router.post("/notice_approve", isAdminLoggedIn, async(req, res) => {
+    const { NoticePending } = req.context.models;
+    var query = {}
+    if (req.body.noticeId) {
+        curNoticeId = req.body.noticeId
+        let notice = await NoticePending.findOne({ noticeId: curNoticeId }).lean();
+        const { Alumni } = req.context.models;
+        var alumniDetails = await Alumni.findOne({ userId: notice.userId }).lean();
+        if (notice) {
+            const { Notice } = req.context.models;
+            delete notice._id;
+            await NoticePending.remove({ noticeId: curNoticeId });
+            const transporter = nodemailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: process.env.MAIL_ID,
+                    pass: process.env.MAIL_PASSWORD
+                }
+            });
+            if (req.body.approved == 0) {
+                const options = {
+                    from: process.env.MAIL_ID,
+                    to: alumniDetails.email,
+                    subject: "Your Notice has been Rejected",
+                    text: "Hi " + alumniDetails.firstName + ",\n Sorry to inform that your Notice titled " + notice.title + " has been Rejected. Kindly Contact the admin to know more.\n\nRegards,\nBeyond Graduation,\nCETAA"
+
+                }
+                transporter.sendMail(options, function(err, info) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log("Sent :" + info.response)
+                })
+                res.send("Notice request rejected for " + alumniDetails.firstName + ".");
+                return;
+            } else {
+                var updatedNotice = await Notice.create(notice);
+                const options = {
+                    from: process.env.MAIL_ID,
+                    to: alumniDetails.email,
+                    subject: "Your Notice has been Approved",
+                    text: "Hi " + alumniDetails.firstName + ",\n Your notice titled " + notice.title + " has been Approved. Thanks for your contribution :)\n\nRegards,\nBeyond Graduation,\nCETAA"
+
+                }
+                transporter.sendMail(options, function(err, info) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log("Sent :" + info.response)
+                })
+                res.send("Notice request approved for " + alumniDetails.firstName + ".");
+                return;
+            }
+
+        }
+        res.json(updatedNotice);
+    }
+});
+
+router.get("/pending_notice_list", isAdminLoggedIn, async(req, res) => {
+    const { NoticePending } = req.context.models;
+    res.json(
+        await NoticePending.find().lean().sort({ dateUploaded: -1, title: 1 }).catch((error) =>
+            res.status(400).json({ error })
+        )
     );
-  });
+});
 
 
 
 
-router.get("/stats", isAdminLoggedIn, async (req, res) => {
+router.get("/stats", isAdminLoggedIn, async(req, res) => {
 
 
-    var counts ={};
+    var counts = {};
     counts.Admin = await (req.context.models.Admin).countDocuments({});
     counts.Alumni = await (req.context.models.Alumni).countDocuments({});
     counts.AlumniPending = await (req.context.models.AlumniPending).countDocuments({});
@@ -230,20 +224,18 @@ router.get("/stats", isAdminLoggedIn, async (req, res) => {
     counts.BlogComments = await (req.context.models.BlogComments).countDocuments({});
     res.json(counts);
 
-  });
+});
 
 
-router.post("/dbrepair", isAdminLoggedIn,async (req, res) => {
-    const {User} = req.context.models;
+router.post("/dbrepair", isAdminLoggedIn, async(req, res) => {
+    const { User } = req.context.models;
     res.json(
-      await User.updateMany({},{dateJoined:Date.now()}).catch((error) =>
-      res.status(400).json({ error })
-    )
+        await User.updateMany({}, { passwordResetToken: "000000000000000" }).catch((error) =>
+            res.status(400).json({ error })
+        )
     )
 });
 
 
 
 module.exports = router;
-
-
--- models/User.js
+++ models/User.js
@@ -1,32 +1,33 @@
-const {Schema, model} = require("../db/connection") // import Schema & model
+const { Schema, model } = require("../db/connection") // import Schema & model
 
 
 // User Schema
 const UserSchema = new Schema({
-    userId:{type: String, unique: true, required: true},
-    email:{type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    firstName:{type: String,required: true},
-    lastName:{type: String,required: true},
-    gender:{
+    userId: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    gender: {
         type: String,
-        required:true,
-        enum:["Male","Female","Transgender","Non-binary/non-conforming","Prefer not to say"]
+        required: true,
+        enum: ["Male", "Female", "Transgender", "Non-binary/non-conforming", "Prefer not to say"]
 
     },
-    profilePicPath:{type:String , default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"},
-    department:{type: String,required: true},
-    areasOfInterest:[String],
-    address:{type: String, default: null},
-    phone:{type: String,required:true},
-    linkedin:{type:String, default: null},
-    github:{type:String, default: null},
-    otherLinks:{type:String, default: null},
-    bookmarkBlog:[String],
-    resume:{type: String,default:""},
+    profilePicPath: { type: String, default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" },
+    department: { type: String, required: true },
+    areasOfInterest: [String],
+    address: { type: String, default: null },
+    phone: { type: String, required: true },
+    linkedin: { type: String, default: null },
+    github: { type: String, default: null },
+    otherLinks: { type: String, default: null },
+    bookmarkBlog: [String],
+    resume: { type: String, default: "" },
     updated: { type: Date, default: Date.now },
-    dateJoined: {type:Date, default: Date.now},
-    likedBlogs:[String]
+    dateJoined: { type: Date, default: Date.now },
+    likedBlogs: [String],
+    passwordResetToken: { type: String, default: "000000000000000", length: 15 },
 })
 
 // // User model
